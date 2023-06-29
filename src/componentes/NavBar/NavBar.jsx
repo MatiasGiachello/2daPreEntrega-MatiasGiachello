68d80b1847913e3d6d7364675e9c4b812f2ea4ca
Matias Giachello <matiasgiachello04@gmail.com>
2023-06-29 17:39:44 -0300

    2da Entrega Rearmando todo

---
+++ src/componentes/NavBar/NavBar.jsx
@@ -0,0 +1,34 @@
+import CartWidget from '../CartWidget/CartWidget'
+import { Link, NavLink } from 'react-router-dom'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <header>
+      <Link to="/">
+        <h1>Mates Artesanales</h1>
+      </Link>
+
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/categoria/2"> Imperiales </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/categoria/3"> Camioneros </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/categoria/4"> Ceramica </NavLink>
+          </li>
+
+        </ul>
+      </nav>
+
+      <CartWidget />
+    </header>
+  )
+}
+
+export default NavBar
